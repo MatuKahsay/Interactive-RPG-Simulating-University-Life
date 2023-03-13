08891d87baab1d66a57e0e8cbc70b0f5286719a9
Stefan-Vo <stefzy10@gmail.com>
2023-03-13 14:58:29 -0700

    Quick fix enemy.h

--- Enemy.h
+++ Enemy.h
@@ -1,6 +1,11 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 #include "stats.h"
+#include "item.h"
+// #include "Player.h"
+
+#include <iostream>
+
 
 class Enemy{
 
@@ -11,10 +16,12 @@ class Enemy{
 
     public: 
         Enemy(const Stats& stat, int ID); 
+        void equipItem(Item& item);
+        void unequipItem(Item& item);
         void attack();
-        int getStrength(const Stats& stats);
-        int getHealth(const Stats& stats);
-        const Stats& getStats() const;  
+        Stats& getStats();  
+        void printStats();
+        // void battle(Player& player, Enemy& enemy);
 
 };
 
