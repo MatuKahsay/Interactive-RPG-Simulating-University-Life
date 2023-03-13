4f255c49937b82cbd002812e2c59fe984941af62
zanderdo <adobm001@ucr.edu>
2023-03-12 21:32:17 -0700

    added updated display functions

---
+++ VS2022attempt/VS2022attempt/UseImGui.cpp
@@ -0,0 +1,41 @@
+#include "UseImGui.h"
+
+void UseImGui::Init(GLFWwindow* window, const char* glsl_version) {
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+	ImGui::StyleColorsDark();
+}
+
+void UseImGui::NewFrame() {
+	//feed inputs to dear imgui, start new frame
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void UseImGui::Update()	{
+	//render your GUI
+	this->NewFrame();
+	ImGui::Begin("Da Window");
+	ImGui::Button("AAAAAAAAA");
+	ImGui::End();
+
+
+}
+
+void UseImGui::Render()	{
+	//Render dear imgui into screen
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void UseImGui::Shutdown() {
+	//Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+}
